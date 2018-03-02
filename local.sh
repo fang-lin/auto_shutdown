a257e5bf19299622689ab2f83f878f0d9f52c6b8
Lin Fang <fang.lin@rea-group.com>
2018-03-02 09:39:20 +0800

    remove space.

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
- #/etc/rc.local.d/local.sh
+#/etc/rc.local.d/local.sh
 /bin/kill $(cat /var/run/crond.pid)
 /bin/echo '*/3  *    *   *   *   /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping_daemon.sh' >> /var/spool/cron/crontabs/root
 /bin/crond
