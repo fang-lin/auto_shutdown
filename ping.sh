dd7586dec85ebbbbbe88d92f91e3d7d7380cf2c6
Lin Fang <fang.lin@rea-group.com>
2018-03-13 00:13:46 +0800

    change the path

--- ping.sh
+++ ping.sh
@@ -4,19 +4,19 @@ while
     ping -c 1 -W 5 192.168.1.1 > /dev/null
     [ $? -eq 0 ];
 do
-    # echo "$(date): Ping is OK." >> /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.log
+    # echo "$(date): Ping is OK." >> /vmfs/volumes/datastore1/auto_shutdown/ping.log
     sleep 3m
 done
 
-echo "$(date): Ping is failure. try again after 1 minute!" >> /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.log
+echo "$(date): Ping is failure. try again after 1 minute!" >> /vmfs/volumes/datastore1/auto_shutdown/ping.log
 sleep 1m
 
 ping -c 1 -W 10 192.168.1.1 > /dev/null
 if [ $? -eq 0 ]
 then
-    echo 'Try again, Ping is OK!' >> /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.log
+    echo 'Try again, Ping is OK!' >> /vmfs/volumes/datastore1/auto_shutdown/ping.log
 else
-    echo 'Ping is failure again, shut down Now!' >> /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.log
+    echo 'Ping is failure again, shut down Now!' >> /vmfs/volumes/datastore1/auto_shutdown/ping.log
     /bin/shutdown.sh
     halt
 fi
--- ping_daemon.sh
+++ ping_daemon.sh
@@ -3,8 +3,8 @@
 NUM=`ps -c | grep ping.sh | grep -v grep | wc -l`
 
 if [ "${NUM}" -lt "1" ];then
-    /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.sh &
-    echo "$(date): Ping daemon start" >> /vmfs/volumes/5a29cc41-b5d585cc-bf15-00fd45fc877c/auto_shutdown/ping.log
+    /vmfs/volumes/datastore1/auto_shutdown/ping.sh &
+    echo "$(date): Ping daemon start" >> /vmfs/volumes/datastore1/auto_shutdown/ping.log
 fi
 
 exit 0
